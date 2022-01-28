c85b7896a22473e97c194ba91a7e2c9d59012fcb
Jordan Sexton <jordan@jordansexton.com>
2022-01-28 15:40:30 -0600

    use DUMMY as USDC on devnet

--- point-of-sale/src/App.tsx
+++ point-of-sale/src/App.tsx
@@ -5,12 +5,12 @@ import { TorusWalletAdapter } from '@solana/wallet-adapter-torus';
 import { PublicKey } from '@solana/web3.js';
 import React, { FC, useMemo } from 'react';
 import { Outlet, useSearchParams } from 'react-router-dom';
-import { SOLIcon } from './components/images/SOLIcon';
+import { USDCIcon } from './components/images/USDCIcon';
 import { ConfigProvider } from './hooks/useConfig';
 import { PaymentProvider } from './hooks/usePayment';
 import { ThemeProvider } from './hooks/useTheme';
 import { TransactionsProvider } from './hooks/useTransactions';
-import { DEVNET_ENDPOINT } from './utils/constants';
+import { DEVNET_DUMMY_MINT, DEVNET_ENDPOINT } from './utils/constants';
 
 export const App: FC = () => {
     const wallets = useMemo(() => [new PhantomWalletAdapter(), new TorusWalletAdapter()], []);
@@ -41,10 +41,11 @@ export const App: FC = () => {
                         <ConfigProvider
                             recipient={recipient}
                             label={label}
-                            symbol="SOL"
-                            icon={<SOLIcon />}
+                            splToken={DEVNET_DUMMY_MINT}
+                            symbol="USDC"
+                            icon={<USDCIcon />}
                             decimals={9}
-                            minDecimals={1}
+                            minDecimals={2}
                             requiredConfirmations={9}
                         >
                             <TransactionsProvider>
